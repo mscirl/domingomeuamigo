f15cc41207b05b9ebf1e89e4aa638559fe5b5034
thais maciel <thaismaciielf@hotmail.com>
2025-03-09 19:28:51 -0300

    feat: add document_reader e diretório stories

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import spacy
 
-nlp = spacy.load("pt_core_news_sm")
+nlp = spacy.load("pt_core_news_md")
+
+texto = "Estou aprendendo a usar o spacy"
+
+doc = nlp(texto)
+
+for sent in doc.sents:
+    print(sent)
+
+
+
+# print("=== Tokens e POS Tags ===")
+# for token in doc:
+#     print(f"Palavra: {token.text:<15} | POS: {token.pos_} | Lemma: {token.lemma_}")
+
+# print("\n=== Entidades Reconhecidas ===")
+# for entidade in doc.ents:
+#     print(f"Entidade: {entidade.text} | Tipo: {entidade.label_}")
---
+++ document_reader.py
@@ -0,0 +1,5 @@
+import docling
+import spacy
+
+from docling.document_converter import DocumentConverter
+
